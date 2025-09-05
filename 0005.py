8174457a3a20af1e9e789d98d5f5a9f89a708f38
moazyoussefmohd-lab <moaz.youssef.mohd@gmail.com>
2025-09-05 19:14:49 +0300

    Update 0005.py

--- 0005.py
+++ 0005.py
@@ -1,23 +1,27 @@
 class Animal:
-    def __init__(self,name,age,species):
+    def __init__(self, name, age, species):
         self.name = name
         self.age = age
         self.species = species
 
 class Dog(Animal):
-    def breed(self,breed):
+    def __init__(self, name, age, species, breed):
+        self.name = name
+        self.age = age
+        self.species = species
         self.breed = breed
 
 class Cat(Animal):
-    def color(self,color):
+    def __init__(self, name, age, species, color):
+        self.name = name
+        self.age = age
+        self.species = species
         self.color = color
-                
-dog_1=Dog("Shalaby", 3, "Dog")
-dog_1.breed("Golden")
+
+dog_1 = Dog("Shalaby", 3, "Dog", "Golden")
 print(dog_1.name, dog_1.age, dog_1.species, dog_1.breed)
 
-
-cat_1=Cat("Tom", 2, "Cat")
-cat_1.color("balck and white")
+cat_1 = Cat("Tom", 2, "Cat", "black and white")
 print(cat_1.name, cat_1.age, cat_1.species, cat_1.color)
+
 
